b7140a31a9c47ae926076b528ff0a4b1cd1ad4d0
Christopher Pezza <chris.pezza@hey.com>
2021-05-14 17:02:16 -0400

    add meta images

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Site Title Here</title>
+        <title>Chiefpansancolt: A Gamer, Father, Husband, and Developer</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Site Description Here" />
+        <meta name="description" content="Chiefpansancolt is a avid gamer, devoted father and husband, a developer changing the world one line of code at a time!" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <div></div>
     </div>
