7deef2b4089909b5bb6a9781d0061633d5d8d56c
Christopher Pezza <chris.pezza@hey.com>
2021-05-16 17:06:45 -0400

    fix base files

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import '@/styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Chiefpansancolt is a avid gamer, devoted father and husband, a developer changing the world one line of code at a time!"
+        />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,8 @@ export default function Home() {
     <div>
       <Head>
         <title>Chiefpansancolt: A Gamer, Father, Husband, and Developer</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Chiefpansancolt is a avid gamer, devoted father and husband, a developer changing the world one line of code at a time!" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/site.webmanifest" />
       </Head>
-      <div></div>
+      <main></main>
     </div>
   )
 }
