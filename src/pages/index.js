3eeef81d2b29c6074284c03042c4c07f13881b1a
Christopher Pezza <chris.pezza@hey.com>
2021-05-24 09:31:04 -0400

    Add Contact section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Hero from '@/components/Hero'
 import Schedule from '@/components/Schedule'
+import Contact from '@/components/Contact'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <main>
         <Hero />
         <Schedule />
+        <Contact />
       </main>
     </div>
   )
