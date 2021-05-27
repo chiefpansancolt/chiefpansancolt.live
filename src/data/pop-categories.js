788b8ebb93e6570e9a31d64661abb9cc86231a8b
Christopher Pezza <chris.pezza@hey.com>
2021-05-27 15:59:35 -0400

    Add starting pop data

---
+++ src/data/pop-categories.js
@@ -0,0 +1,17 @@
+const popCategories = [
+  { name: 'Marvel', image: 'marvel.png', selected: false},
+  { name: 'Game of Thrones', image: 'game-of-thrones.png', selected: false},
+  { name: 'Harry Potter', image: 'harry-potter.png', selected: false},
+  { name: 'Star Wars', image: 'star-wars.png', selected: false},
+  { name: 'Disney', image: 'disney.png', selected: false},
+  { name: 'Fortnite', image: 'fortnite.jpeg', selected: false},
+  { name: 'Pokemon', image: 'pokemon.png', selected: false},
+  { name: 'Jurassic Park', image: 'jurassic-park.png', selected: false},
+  { name: 'Borderlands', image: 'boarderlands.png', selected: false},
+  { name: 'Stanger Things', image: 'stranger-things.png', selected: false},
+  { name: 'DC', image: 'dc.png', selected: false},
+  { name: 'Kingdom Hearts', image: 'kingdom-hearts.png', selected: false},
+  { name: 'Fast & Furious', image: 'fast-and-furious.png', selected: false}
+]
+
+export default popCategories
