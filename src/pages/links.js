a1473c7fa7587961c9d3eb98249cd79eda962026
Christopher Pezza <chris.pezza@hey.com>
2021-05-27 15:47:43 -0400

    Add Page Heading

--- src/pages/links.js
+++ src/pages/links.js
@@ -1,5 +1,6 @@
 import links from '@/data/links'
 import { ExternalLinkIcon } from '@heroicons/react/solid'
+import PageHeading from '@/components/PageHeading'
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -8,17 +9,12 @@ function classNames(...classes) {
 export default function Links() {
   return (
     <div className="bg-gray-100">
-      <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-        <div className="text-center">
-          <h2 className="text-base font-semibold text-indigo-600 tracking-wide uppercase">Links</h2>
-          <p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-            External Links
-          </p>
-          <p className="max-w-xl mt-5 mx-auto text-xl text-gray-500">
-            Check out all the different links to other sites that chief is active on for various
-            items on the internet.
-          </p>
-        </div>
+      <PageHeading
+        title="Links"
+        heading="External Links"
+        subHeading="Check out all the different links to other sites that chief is active on for various items on the internet."
+      />
+      <div className="max-w-7xl mx-auto pb-16 px-4 sm:pb-24 sm:px-6 lg:px-8">
         <div>
           {links.map((group) => (
             <div key={group.name} className="pt-6">
