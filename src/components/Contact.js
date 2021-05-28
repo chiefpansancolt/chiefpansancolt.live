8b04ebdcc52c6e1b0da71c7f0832136769845d88
Christopher Pezza <chris.pezza@hey.com>
2021-05-27 23:57:34 -0400

    add id value to contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import { MailIcon } from '@heroicons/react/outline'
 
 export default function Contact() {
   return (
-    <div className="bg-gray-100">
+    <div className="bg-gray-100" id="contact">
       <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
         <div className="relative bg-white shadow-xl">
           <h2 className="sr-only">Contact us</h2>
