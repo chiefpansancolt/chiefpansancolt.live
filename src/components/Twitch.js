a497edf129dc8e2967907452e0bc93e72f31e3aa
Christopher Pezza <chris.pezza@hey.com>
2023-03-25 09:04:15 -0400

    update twitch embed usage

--- src/components/Twitch.js
+++ src/components/Twitch.js
@@ -1,22 +1,23 @@
-import { useEffect } from 'react'
-const EMBED_URL = 'https://embed.twitch.tv/embed/v1.js'
+import { useRef } from 'react'
+import { TwitchEmbed } from 'react-twitch-embed'
 
 export default function Twitch() {
-  useEffect(() => {
-    const script = document.createElement('script')
-    script.setAttribute('src', EMBED_URL)
-    script.addEventListener('load', () => {
-      new window.Twitch.Embed('twitch-embed', {
-        width: '100%',
-        height: 500,
-        channel: 'chiefpansancolt',
-        parent: ['localhost'],
-      })
+  const embed = useRef()
 
-      document.getElementsByTagName('IFrame')[0].classList.add('rounded-lg')
-    })
-    document.body.appendChild(script)
-  }, [])
+  const handleReady = (e) => {
+    embed.current = e
+  }
 
-  return <div id="twitch-embed"></div>
+  return (
+    <TwitchEmbed
+      channel="chiefpansancolt"
+      autoplay
+      muted
+      withChat
+      darkMode={true}
+      hideControls
+      onVideoReady={handleReady}
+      width="100%"
+    />
+  )
 }
