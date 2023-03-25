a6eb7133f7f7df336b029e396a031cd28394d581
Christopher Pezza <chris.pezza@hey.com>
2023-03-25 09:03:14 -0400

    Remove Pops usage

--- src/components/RightColumn.js
+++
@@ -1,9 +0,0 @@
-export default function RightColumn({ children }) {
-  return (
-    <div className="bg-gray-100 pr-4 sm:pr-6 lg:pr-8 lg:flex-shrink-0 lg:border-l lg:border-gray-200 xl:pr-0">
-      <div className="h-full pl-6 pb-6 lg:w-80">
-        <div className="h-full relative min-h-[16rem]">{children}</div>
-      </div>
-    </div>
-  )
-}
--- src/hooks/groupCount.js
+++
@@ -1,5 +0,0 @@
-import _ from 'lodash'
-export function groupCount(groupName, pops) {
-  const data = _.groupBy(pops, ['group', groupName])
-  return data.true ? data.true.length : 0
-}
