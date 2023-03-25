70acdc242d644edb2bf5f37889dcc6d1073c50fe
Christopher Pezza <chris.pezza@hey.com>
2023-03-25 09:04:34 -0400

    update stats usage

--- src/data/stats.js
+++ src/data/stats.js
@@ -2,32 +2,32 @@ const stats = [
   {
     name: 'Twitter',
     href: 'https://twitter.com/chiefpansancolt',
-    count: 124,
+    count: 121,
   },
   {
     name: 'Instagram',
     href: 'https://instagram.com/chiefpansancolt',
-    count: 3,
+    count: 18,
   },
   {
-    name: 'eFuse',
-    href: 'https://efuse.gg/u/chiefpansancolt',
-    count: 155,
+    name: 'TikTok',
+    href: 'https://tiktok.com/@chiefpansancolt',
+    count: 120,
   },
   {
     name: 'YouTube (Gaming)',
     href: 'https://youtube.com/channel/UCDiSj1nIsOnMHo3IASiN-3w',
-    count: 92,
+    count: 111,
   },
   {
     name: 'YouTube (Shorts)',
     href: 'https://www.youtube.com/channel/UCqHORCP5Eq7SpsyLKE-V2Zg',
-    count: 7,
+    count: 1,
   },
   {
     name: 'Twitch',
     href: 'https://twitch.tv/chiefpansancolt',
-    count: 82,
+    count: 84,
   },
 ]
 
